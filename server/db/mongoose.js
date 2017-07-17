29a0dd3ebf5d3797c0621e8c8333929f74ece242
Alexis Duclos <alexis.duclos@gmail.com>
2017-07-17 07:00:40 +0200

    Add test on db connect string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,12 @@
+var os = require('os');
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://gnoume:pouet@ds147872.mlab.com:47872/todoapp');
+
+if (os.hostname() == 'LT-0007'){
+    mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
+} else {
+    mongoose.connect('mongodb://gnoume:pouet@ds147872.mlab.com:47872/todoapp');
+}
 
 module.exports = { mongoose };
