9ac58a76d1d32aaea1753a801f2b8e7273374071
Alexis Duclos <alexis.duclos@gmail.com>
2017-07-23 06:44:06 +0200

    Setup separate test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,6 @@
-var os = require('os');
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-if (os.hostname() == 'LT-0007'){
-    mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
-} else {
-    mongoose.connect('mongodb://gnoume:pouet@ds147872.mlab.com:47872/todoapp');
-}
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
